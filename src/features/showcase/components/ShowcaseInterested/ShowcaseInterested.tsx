4212db627fcb45408fa08a89d9dc2d6f786b9c08
jotaaloud <jotaaloud@gmail.com>
2026-02-07 20:45:04 +0100

    <migration>(`SHOWCASE`): completed. Updates skills.

---
+++ src/features/showcase/components/ShowcaseInterested/ShowcaseInterested.tsx
@@ -0,0 +1,13 @@
+export default function showcaseInterested() {
+    return (
+        <section>
+            <details>
+                <summary>🧠Learning and interested on:</summary>
+                <img className="badge" src="https://img.shields.io/badge/%F0%9F%A4%96-Artificial%20Intelligence-4c2271?style=for-the-badge&labelColor=lightgrey" alt="Artificial intelligence" />
+                <img className="badge" src="https://img.shields.io/badge/GoogleCloud-%234285F4.svg?style=for-the-badge&logo=google-cloud&logoColor=white" alt="Google Cloud"></img>
+                <img className="badge" src="https://img.shields.io/badge/%F0%9F%93%B1-Mobile%20development-4c2271?style=for-the-badge&labelColor=lightgrey" alt="Mobile Development" />
+                <img className="badge" src="https://img.shields.io/badge/Three.js-000000?style=for-the-badge&logo=three.js&logoColor=white" alt="Three.js" />
+            </details>
+        </section>
+    )
+}
---
+++ src/features/showcase/components/ShowcaseTechnologies/ShowcaseTechnologies.tsx
@@ -0,0 +1,16 @@
+export default function showcaseTechnologies() {
+    return (
+        <section>
+            <h2>🛠️ Technologies:</h2>
+            <img className="badge" src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54" alt="Python" />
+            <img className="badge" src="https://img.shields.io/badge/kotlin-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white" alt="Kotlin" />
+            <img className="badge" src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white" alt="TypeScript" />
+            <img className="badge" src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL" />
+
+            <img className="badge" src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white" alt="" />
+            <img className="badge" src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="" />
+            <img className="badge" src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="" />
+            <img className="badge" src="https://img.shields.io/badge/VB-Visual_Basic-325a99?style=for-the-badge&logoColor=white&labelColor=1c3969" alt="" />
+        </section>
+    )
+}
